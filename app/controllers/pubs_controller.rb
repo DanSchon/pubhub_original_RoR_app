5a71820493a3d17c279f7bc5833a6f2145fad954
Daniel Schonfeld <danyschonfeld@gmail.com>
2016-02-15 13:08:39 -0800

    implemented ability for users to upload posts using TDD

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -3,7 +3,16 @@ class PubsController < ApplicationController
     @pubs = Pub.all
   end
   def new
+    @pub = Pub.new
   end
   def create
+    @pub = Pub.new(params.require(:pub).permit(:title, :url, :content))
+    @pub.user = current_user
+    if @pub.save
+      redirect_to home_path
+    else
+      flash[:error] = "All fields are required!"
+      render :new
+    end
   end
 end
--- spec/controllers/pubs_controller_spec.rb
+++ spec/controllers/pubs_controller_spec.rb
@@ -9,4 +9,42 @@ describe PubsController do
       expect(assigns(:pubs)).to match_array([pub1, pub2])
     end
   end
+  describe "GET new" do 
+    it "sets @pub" do 
+      get :new
+      expect(assigns(:pub)).to be_instance_of(Pub)
+    end
+  end
+  describe "POST create" do
+    context "with valid input" do 
+      it "redirects to home page" do 
+        post :create, pub: Fabricate.attributes_for(:pub)
+        expect(response).to redirect_to home_path
+      end
+      it "creates a pub" do 
+        post :create, pub: Fabricate.attributes_for(:pub)
+        expect(Pub.count).to eq(1)
+      end
+      it "creates a pub associated with the current_user" do 
+        user = Fabricate(:user)
+        session[:user_id] = user.id 
+        post :create, pub: Fabricate.attributes_for(:pub), user_id: user.id
+        expect(Pub.first.user).to eq(user)
+      end
+    end
+    context "with invalid input" do 
+      it "does not create a pub" do
+        post :create, pub: {title: "title"}
+        expect(Pub.count).to eq(0)
+      end
+      it "renders new template" do
+        post :create, pub: {title: "title"}
+        expect(response).to render_template :new
+      end
+      it "sets notice" do 
+        post :create, pub: {title: "title"}
+        expect(flash[:error]).to_not be_empty
+      end
+    end
+  end
 end
